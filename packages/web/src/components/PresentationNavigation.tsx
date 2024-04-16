9f15b208d655104291a8840c89ab8e75ff40f6b7
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2024-04-16 21:22:11 +0700

    Peculiarnewbie/fro 19 send current presenter position on join (#28)
    * send presenter update when someone joins
    
    * move to a function
    
    * Revert "move to a function"
    
    This reverts commit 879cd43da6b5aa1ea4466bde19ab7f775046fb5d.
    
    * not using props

--- packages/web/src/components/PresentationNavigation.tsx
+++ packages/web/src/components/PresentationNavigation.tsx
@@ -81,6 +81,7 @@ export default function PresentationNavigation(props: {
 		slideTracker.client = props.slide;
 		if (webSocket && props.isPresenter) {
 			sendSlideUpdate(webSocket);
+			slideTracker.presenter = props.slide;
 		}
 	});
 
@@ -100,8 +101,22 @@ export default function PresentationNavigation(props: {
 
 			console.log(data);
 
-			if ("type" in data && data.type === "slide") {
-				followPresenter(data);
+			if ("type" in data) {
+				if (data.type === "slide") followPresenter(data);
+				else if (data.type === "update") {
+					slideTracker.presenter = data.slide;
+					console.log("updated", data.slide);
+				} else if (data.type === "join") {
+					if (props.isPresenter) {
+						console.log("sending update", slideTracker.presenter);
+						webSocket?.send(
+							JSON.stringify({
+								type: "update",
+								slide: slideTracker.presenter,
+							})
+						);
+					}
+				}
 			}
 
 			//TODO: after join message update slideTracker.presenter
